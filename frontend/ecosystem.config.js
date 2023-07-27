a82497409481919886d2e91a128a9364e4294647
adolph liu <yul5@cs.cmu.edu>
2023-07-27 22:23:46 +0800

    deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "47.106.245.77",
       ref: "origin/master",
       repo: "git@github.com:LYZJU2019/diary-book.git",
-      path: "~/diary-book/frontend",
+      path: "/root/diary-book/frontend",
       "post-deploy":
         "git reset --hard && git checkout master && git pull && nvm use 16.20.1 && npm i --production=false && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
       env: {
