bddf614f315a6f63f99b55b36eb95344ffabb35e
adolph liu <yul5@cs.cmu.edu>
2023-07-27 22:17:09 +0800

    deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'juejue-vite-h5',
-      script: 'juejue-vite-h5-server.js'
+      name: "juejue-vite-h5",
+      script: "juejue-vite-h5-server.js",
     },
   ],
   deploy: {
     production: {
-      user: 'root',
-      host: '47.106.245.77',
-      ref: 'origin/master',
-      repo: 'git@github.com:LYZJU2019/diary-book.git',
-      path: '/workspace/juejue-vite-h5',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      user: "root",
+      host: "47.106.245.77",
+      ref: "origin/master",
+      repo: "git@github.com:LYZJU2019/diary-book.git",
+      path: "/workspace/juejue-vite-h5",
+      "post-deploy":
+        "git reset --hard && git checkout master && git pull && nvm use 16.20.1 && npm i --production=false && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
       env: {
-        NODE_ENV: 'production'
-      }
-    }
-  }
-}
+        NODE_ENV: "production",
+      },
+    },
+  },
+};
