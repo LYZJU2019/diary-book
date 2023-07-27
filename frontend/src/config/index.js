bdada29d32b1d1e4d1fed7483eebca3db1ef4a0d
adolph liu <yul5@cs.cmu.edu>
2023-07-27 22:34:10 +0800

    deploy

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,4 @@
 const MODE = import.meta.env.MODE; // 环境变量
 
 export const baseUrl =
-  MODE == "development" ? "http://127.0.0.1:7009" : "http://api.chennick.wang";
+  MODE == "development" ? "http://127.0.0.1:7009" : "http://47.106.245.77:7009";
